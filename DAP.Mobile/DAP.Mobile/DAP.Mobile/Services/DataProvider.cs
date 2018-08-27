dfcdb57b1bbcc52d2324d9efb0000a8fb263140c
Pablo Pereyra Molina <pablopm@lagash.com>
2018-08-26 22:14:24 -0300

    pantalla planificacion diaria

--- DAP.Mobile/DAP.Mobile/DAP.Mobile/Services/DataProvider.cs
+++ DAP.Mobile/DAP.Mobile/DAP.Mobile/Services/DataProvider.cs
@@ -5,12 +5,14 @@ namespace DAP.Mobile.Services
 {
     public static class DataProvider
     {
+        public static IList<Periodicity> DailyPeriodicities { get; private set; }
         public static IList<Periodicity> Periodicities { get; private set; }
         public static IList<Pill> Pills { get; private set; }
 
         static DataProvider()
         {
             FillPeriodicities();
+            FillDailyPeriodicities();
             FillPills();
         }
 
@@ -30,6 +32,17 @@ namespace DAP.Mobile.Services
                 new Periodicity { Id = 1, Description = "Diaria", NextPage = "DailyPlanificationPage" },
                 new Periodicity { Id = 2, Description = "Semanal", NextPage = "WeeklyPlanificationPage" },
                 new Periodicity { Id = 3, Description = "Personalizada", NextPage = "CustomPlanificationPage" }
+            };
+        }
+
+        private static void FillDailyPeriodicities()
+        {
+            DailyPeriodicities = new List<Periodicity>()
+            {
+                new Periodicity { Id = 1, Description = "Cada 6 hs" },
+                new Periodicity { Id = 2, Description = "Cada 8 hs" },
+                new Periodicity { Id = 3, Description = "Cada 12 hs" },
+                new Periodicity { Id = 4, Description = "Cada 24 hs" }
             };
         }
     }
