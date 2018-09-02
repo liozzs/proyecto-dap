e29f22adbfb3715856e0804f0ac51f4f2265fb59
Lionel Yoshida <liozzs@gmail.com>
2018-09-01 23:11:12 -0300

    smart config

--- DAP.Arduino/wifi/wifi.ino
+++ DAP.Arduino/wifi/wifi.ino
@@ -19,9 +19,43 @@ String prev_get = "";
 //NTP
 NTPClient *ntp;
 
+//Server app mobile
+ESP8266WebServer  server(80);
+int reqCount = 0;                // number of requests received
+
 void setup() {
   Serial.begin(115200);
+
+//Smart config
+
+  if (!WiFi.setAutoConnect(true)) {
+  /* Set ESP32 to WiFi Station mode */
+    WiFi.mode(WIFI_STA);
+    /* start SmartConfig */
+    WiFi.beginSmartConfig();
+    delay(1000);
+    /* Wait for SmartConfig packet from mobile */
+    Serial.println("Waiting for SmartConfig.");
+    while (!WiFi.smartConfigDone()) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("SmartConfig done.");
   
+    /* Wait for WiFi to connect to AP */
+    Serial.println("Waiting for WiFi");
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("WiFi Connected.");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+  }
+
+//Wifi Manager deshabilitado  
+/*  
   //Inicialización WiFi
   WiFiManager wifiManager;
   wifiManager.setTimeout(180);
@@ -42,6 +76,8 @@ void setup() {
 
   debug("WIFI: connected...\n");
 
+
+*/
   ntp = new NTPClient();
 
   //enviar a arduino primer mensaje con el horario correcto e IP publica
@@ -68,11 +104,35 @@ void setup() {
   root.printTo(Serial);
 
 
+  // start the web server on port 80
+  server.on("/", handleRoot);               // Call the 'handleRoot' function when a client requests URI "/"
+  server.onNotFound(handleNotFound);        // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
+  server.begin();
+
 }
 
 void loop() {
-  delay(10000); 
+
+  //SERVER
+  
+  server.handleClient();  
+
+  
 }
 
 
+void handleRoot() {
+  //String arg = server.arg("plan");
+  //Serial.println(arg);
+  StaticJsonBuffer<350> newBuffer;
+  JsonObject& root = newBuffer.parseObject(server.arg("plain"));
+  root["plan"] = true;
+  root.printTo(Serial);
+
+  server.send(200, "text/plain", "Hello world!");   // Send HTTP status 200 (Ok) and send some text to the browser/client
+}
+
+void handleNotFound(){
+  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+}
 
